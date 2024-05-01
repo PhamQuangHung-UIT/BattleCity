41ab82ae65fbff20a9fa962091821258801c73c1
Phạm Quang Hưng <21522123@ms.uit.edu.vn>
2024-05-02 01:03:51 +0700

    Initialize libGDX desktop project for BattleCity

---
+++ desktop/src/com/uit/battlecity/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.uit.battlecity;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.uit.battlecity.BattleCity;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Battle City");
+		new Lwjgl3Application(new BattleCity(), config);
+	}
+}
